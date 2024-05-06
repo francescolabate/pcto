2a6058fad077d94bb7c7b8117333851dfbfd2d59
francescolabate <169042609+francescolabate@users.noreply.github.com>
2024-05-06 14:55:26 +0000

    ciao

---
+++ ex00.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=3;
+
+    int b=4;
+
+    float c=2.5;
+    float d=3.1;
+
+    double e=2.75;
+    double  f=3.15;
+
+    printf("a+b: %d\n, a+b");
+     printf("a-b: %d\n, a-b");
+      printf("a/b: %d\n, a/b");
+       printf("a*b: %d\n, a*b");
+        
+    printf("c+d %f\n c+d");
+    printf("c+d %f\n c+d");
+    printf("c+d %f\n c+d");
+     printf("c+d %f\n c+d");
+
+     printf("e+f:%lf\n")
+
+    
+
+}
